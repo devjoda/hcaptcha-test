46925bb3b621436d68d5a5518414d1d8d28b4c21
devjoda <dev.joda@gmail.com>
2023-02-07 21:40:47 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,50 +1,45 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const fetch = require('node-fetch')
-const { verify } = require("hcaptcha");
+import * as dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
 
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(cors())
+dotenv.config();
+const app = express();
 
-app.post("/signup", async (req, res, next) => {
-    // const token = req.body['h-captcha-response'];
-    const token = req.body.token;
-    const secret = process.env.hcaptchaSecret;
-    const verifyUrl = 'https://hcaptcha.com/siteverify';
-    if (!token) {
-        return res.status(400).json({ error: "Token is missing" });
-    }
+app.use(express.static('dist'));
+app.use(express.json());
+app.use(cors());
 
-    try {
-        const params = new URLSearchParams();
-        params.append('secret', secret);
-        params.append('response', token);
+app.post('/signup', async (req, res) => {
+  const { token } = req.body;
+  const secret = process.env.hcaptchaSecret;
+  const verifyUrl = 'https://hcaptcha.com/siteverify';
+  if (!token) {
+    return res.status(400).json({ error: 'Token is missing' });
+  }
 
-        const response = await fetch(verifyUrl, {
-            method: 'POST',
-            body: params,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-        });
-        
-        const json = await response.json();
-        const { success } = json;
-        if (success) {
-            return res.json({ success: true });
-        } else {
-            return res.status(400).json({ error: "Invalid Captcha" });
-        }
-    } catch (e) {
-        return res.status(400).json({ error: e.response });
-    }
+  const params = new URLSearchParams();
+  params.append('secret', secret);
+  params.append('response', token);
+
+  const response = await fetch(verifyUrl, {
+    method: 'POST',
+    body: params,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+
+  const json = await response.json();
+  const { success } = json;
+  if (success) {
+    return res.json({ success: true });
+  }
+  return res.status(400).json({ error: res.error.data.response });
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${ PORT }...`)
-})
+  console.log(`App listening on port ${PORT}...`);
+});
